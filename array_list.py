c45d9d1ba8ba3f78bb7c06dae17ebb117d93ad58
Derrick Kiprop <derrickip34@gmail.com>
2024-01-14 19:04:57 +0300

    add a sorting function

--- array_list.py
+++ array_list.py
@@ -5,3 +5,14 @@ def generate_values(size):
     for i in range(size):
         array_list.append(random.randrange(1, 100))
     return array_list
+
+def swap_values(array, a, b):
+    temp = array[a]
+    array[a] = array[b]
+    array[b] = temp
+
+def sort_array(array):
+    for i in range(len(array)-1):  # Correcting the loop condition
+        if array[i] > array[i+1]:
+            swap_values(array, i, i+1)
+    return array
