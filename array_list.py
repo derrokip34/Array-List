7004e04f8592b1ca247c74ecfcafa0310aaf68dc
Derrick Kiprop <derrickip34@gmail.com>
2024-01-15 15:26:34 +0300

    create class that encapsulates the array list functions

--- array_list.py
+++ array_list.py
@@ -1,68 +1,66 @@
 import random
 import time
 
-def generate_values(size):
-    array_list = []
-    for i in range(size):
-        array_list.append(random.randrange(1, 100))
-    return array_list
+class ArrayList:
+    def __init__(self, size):
+        self.array_list = [random.randrange(1, 100) for _ in range(size)]
 
-def swap_values(array, a, b):
-    temp = array[a]
-    array[a] = array[b]
-    array[b] = temp
+    def swap_values(self, a, b):
+        temp = self.array_list[a]
+        self.array_list[a] = self.array_list[b]
+        self.array_list[b] = temp
 
-def sort_array(array):
-    for i in range(len(array)):
-        for j in range(len(array)):
-            if array[i] < array[j]:
-                swap_values(array, i, j)
-    return array
+    def sort_array(self):
+        for i in range(len(self.array_list)):
+            for j in range(len(self.array_list)):
+                if self.array_list[i] < self.array_list[j]:
+                    self.swap_values(i, j)
 
-def linear_search(array_list,target):
-    for i in range(len(array_list)):
-        if array_list[i] == target:
-            return True
-    return False
+    def linear_search(self, target):
+        for i in range(len(self.array_list)):
+            if self.array_list[i] == target:
+                return True
+        return False
 
-def binary_search(array, target):
-    low, high = 0, len(array) - 1
-    while low <= high:
-        mid = (low + high) // 2
-        if array[mid] == target:
-            return True
-        elif array[mid] < target:
-            low = mid + 1
+    def binary_search(self, target):
+        low, high = 0, len(self.array_list) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if self.array_list[mid] == target:
+                return True
+            elif self.array_list[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
+
+    def repeat_search_function(self, search_method, target_value, iterations):
+        total_time = 0
+        for _ in range(iterations):
+            start = time.time()
+            search_result = search_method(target_value)
+            end = time.time()
+            total_time += (end - start)
+
+        if search_result:
+            print(f"{target_value} found")
         else:
-            high = mid - 1
-    return False
+            print(f"{target_value} not found")
 
-def repeat_search_function(search_method,array_list,target_value,iterations):
-    total_time = 0
-    for _ in range(iterations):
-        start = time.time()
-        search_result =  search_method(array_list,target_value)
-        end = time.time()
-        total_time += (end - start)
-    if search_result:
-        print(f"{target_value} found")
-    else:
-        print(f"{target_value} not found")
-    
-    avg_time = total_time/iterations
+        avg_time = total_time / iterations
 
-    return avg_time
+        return avg_time
 
 if __name__ == "__main__":
-    list_sizes = [100,300,800]
-    for i in range(len(list_sizes)):
-        array_list = generate_values(list_sizes[i])
+    list_sizes = [100, 300, 800]
+    for size in list_sizes:
+        array_list_obj = ArrayList(size)
 
-        target_value = random.choice(array_list)
+        target_value = random.choice(array_list_obj.array_list)
 
-        linear_search_time = repeat_search_function(linear_search, array_list, target_value, 1000)
-        print(f"Linear Search Time for size {list_sizes[i]}: {linear_search_time:.10f} seconds")
-        
-        sorted_array = sort_array(array_list)
-        binary_search_time = repeat_search_function(binary_search, sorted_array, target_value, 1000)
-        print(f"Binary Search Time for size {list_sizes[i]}: {binary_search_time:.10f} seconds")
+        linear_search_time = array_list_obj.repeat_search_function(array_list_obj.linear_search, target_value, 1000)
+        print(f"Linear Search Time for size {size}: {linear_search_time:.10f} seconds")
+
+        array_list_obj.sort_array()
+        binary_search_time = array_list_obj.repeat_search_function(array_list_obj.binary_search, target_value, 1000)
+        print(f"Binary Search Time for size {size}: {binary_search_time:.10f} seconds")
